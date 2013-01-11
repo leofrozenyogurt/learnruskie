c1bcdc102359e6709563508ed87a18bee0e909ca
Leo Royzengurt <rebellabs@gmail.com>
2013-01-11 11:41:17 -0500

    first commit comrades

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learnruskie::Application.config.secret_token = 'c4ccacdbb1d12d753fb4f57bf6b3aea9540aa05aa31fc1ea5118d3fa60075c03bbfbbbdd500d5a045f79a87cf702f660e643e69b7aa60b7a1269e0f95413b66f'
